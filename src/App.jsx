8a29d63d4cd36ad8675a90624b1ed7affaa941af
Juliana Prieto <juliana.prieto@nubank.com.br>
2026-03-18 22:53:33 -0300

    feat: add subtitle to App component

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Hola chicos!</h1>
+          <h2>Cualquier cosa!!!!!</h2>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
